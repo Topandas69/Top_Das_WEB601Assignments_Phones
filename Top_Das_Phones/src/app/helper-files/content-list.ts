30172c22583add7ebe794c886e5da3c97f088754
Topandas69 <W0790712@myscc.ca>
2023-05-14 14:21:03 -0400

    list of contents

---
+++ Top_Das_Phones/src/app/helper-files/content-list.ts
@@ -0,0 +1,33 @@
+import { Content } from "./content-interface";
+
+export class ContentList {
+    private contents: Content[];
+
+    constructor(){
+        this.contents = []
+    }
+
+    get get(){
+        return this.contents;
+    }
+
+    add(content: Content){
+        this.contents.push(content);
+    }
+
+    getLength(){
+        return this.contents.length;
+    }
+
+    getContent(index: number){
+        let content = this.contents[index];
+        return `
+        <div class=''>
+            <h1>${content.title}</h1>
+            <img src="${content.imgURL}">
+            <p>${content.description}</p>
+            <p>Creator: ${content.creator}</p>
+            <p>Type: ${content.type}</p>
+        </div> <hr/>`;
+    }
+}
